06aeef67f7b0fd5854b1695ce8773cf4bd8d8034
Mengistu Dagnaw <mengedagnaw@gmail.com>
2026-01-04 00:23:15 +0100

    Create SR550.m

---
+++ MTEX MATLAB scripts/KSNW/SR550.m
@@ -0,0 +1,9 @@
+rootDir = "/MATLAB Drive/SDSS 2507_EBSD";
+condFolder = "05_SR550_hex";
+outDir = rootDir + "/OR_KS_NW_outputs/SR550";
+tolDeg = 5;
+
+opts = struct(); opts.grainAngDeg = 5; opts.doMaps = true; opts.doHists = true;
+
+T = KSNW_core(rootDir, condFolder, outDir, tolDeg, opts);
+disp(T);
